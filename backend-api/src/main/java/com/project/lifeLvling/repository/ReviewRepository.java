8c67d002d9b1b14e0ca2736ab190b61dd772181a
IsaiahTTT <itturner@uncg.edu>
2026-03-24 01:28:55 -0400

    Review endpoints complete

--- backend-api/src/main/java/com/project/lifeLvling/repository/ReviewRepository.java
+++ backend-api/src/main/java/com/project/lifeLvling/repository/ReviewRepository.java
@@ -1,5 +1,14 @@
 package com.project.lifeLvling.repository;
 
-public class ReviewRepository {
-    
+import com.project.lifeLvling.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByCourseId(Long courseId);
+    List<Review> findByUsername(String username);
+    List<Review> findByRating(int rating);
 }
